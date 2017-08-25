8bf996862b682c19812ad5b48d9c9c5fce97ff5f
Peter L. Morrell <pmorrell@umn.edu>
2017-08-24 22:43:16 -0500

    redesigned script

--- Analysis_Scripts/Data_Handling/Alchemy_Calls.sh
+++ Analysis_Scripts/Data_Handling/Alchemy_Calls.sh
@@ -9,14 +9,16 @@ set -o pipefail
 #    Load the version of Alchemy install on MSI
 module load alchemy_ML
 
-#    Create an array listing each of our directories
-#    Cheating a little, we entered a specific set of plates, will have to change when we genotype other samples!
-genotyping_plates=(2006BOPA1_BA_Plate7 2006BOPA1_MN_Plate3 2006BOPA2_BA_Plate7 2006BOPA2_MN_Plate3 2007BOPA1_BA_Plate7 2007BOPA1_MN_Plate3 2007BOPA1_N6_Plate9 2007BOPA2_BA_Plate7 2007BOPA2_MN_Plate3 2007BOPA2_N6_Plate9 2008BOPA1_BA_Plate7 2008BOPA2_BA_Plate7)
+#    Directory containing intensity files
+INTENSITY_DIR=${HOME}/Alchemy/Alchemy_Intensities/
+INPUT_DIR=${HOME}/Alchemy/Alchemy_Inputs
+OUT_DIR=${HOME}/Alchemy/Alchemy_Outputs
 
-#    Iterate over the array of directories
+declare -a INTENSITIES=($(find "${INTENSITY_DIR}" -name "GS*Custom_BOPAC.txt"))
+INTENSITY_PATH=$(dirname "$INTENSITIES[0]")
 
-for i in "${genotyping_plates[@]}"
-    do
-        echo "$i"
-        alchemy -f "GS_${i}_Custom.txt" -s "${i}_samp_map.txt" -m "${i}_snp_map.txt" --illumina --log="${i}_log.txt" > ./${i}_calls.txt
+for i in "${INTENSITIES[@]}"
+	do
+		sample=$(basename "${i}" | cut -d _ -f 1,2,3,4)
+	    	alchemy -f "${INTENSITY_PATH}/${sample}" -s "${INPUT_DIR}/${sample}_samp_map.txt" -m "${INPUT_DIR}/{sample}_snp_map.txt" --log="$OUT_DIR/${sample}_log.txt" --illumina > "${OUT_DIR}/${sample}_calls.txt"
     done
