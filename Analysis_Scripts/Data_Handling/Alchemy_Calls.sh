6e7f4c1f930d13c70e22a8add023fc116a1b9d47
Peter L. Morrell <pmorrell@umn.edu>
2017-08-25 10:05:17 -0500

    cleaning syntax

--- Analysis_Scripts/Data_Handling/Alchemy_Calls.sh
+++ Analysis_Scripts/Data_Handling/Alchemy_Calls.sh
@@ -14,11 +14,17 @@ INTENSITY_DIR=${HOME}/Alchemy/Alchemy_Intensities/
 INPUT_DIR=${HOME}/Alchemy/Alchemy_Inputs
 OUT_DIR=${HOME}/Alchemy/Alchemy_Outputs
 
+#    Alchemy intensities files have a name like "GS_2006BOPA1_BA_Plate7_Custom_BOPAC.txt"
+#    Saving the name and path to an array
 declare -a INTENSITIES=($(find "${INTENSITY_DIR}" -name "GS*Custom_BOPAC.txt"))
+#    Taking the path name from the 1st entry in the array
 INTENSITY_PATH=$(dirname "${INTENSITIES[0]}")
 
+#    Iterate over the array of intensity files, and for each sample, cut down the path to the base name
+#    The combination of basename and cut return only "GS_2006BOPA1_BA_Plate7"
+#    The path is then built back up to reach each sample
 for i in "${INTENSITIES[@]}"
 	do
 		sample=$(basename "${i}" | cut -d _ -f 1,2,3,4)
-	    	alchemy -f "${INTENSITY_PATH}/${sample}" -s "${INPUT_DIR}/${sample}_samp_map.txt" -m "${INPUT_DIR}/{sample}_snp_map.txt" --log="$OUT_DIR/${sample}_log.txt" --illumina > "${OUT_DIR}/${sample}_calls.txt"
+	    	alchemy -f "${INTENSITY_PATH}/${sample}" -s "${INPUT_DIR}/${sample}_samp_map.txt" -m "${INPUT_DIR}/${sample}_snp_map.txt" --log="$OUT_DIR/${sample}_log.txt" --illumina > "${OUT_DIR}/${sample}_calls.txt"
     done
