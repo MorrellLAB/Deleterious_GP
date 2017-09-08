bf2988cc9d1a094b1ce242477536d4ec0b69eb13
Emily Vonderharr <emilyvonderharr@agro200953237.cfans.umn.edu>
2017-09-08 15:55:47 -0500

    FIlled in file paths to msi file directory

--- Analysis_Scripts/Data_Handling/Genome_Studio_Rename.sh
+++ Analysis_Scripts/Data_Handling/Genome_Studio_Rename.sh
@@ -7,13 +7,13 @@ set -e
 set -o pipefail
 
 #    A SNP translation table list various SNP names
-TS_TABLE=/Users/emilyvonderharr/Documents/Github/Deleterious_GP/Data/BOPA_Name_Translation.csv
+TS_TABLE=/home/morrellp/vonde026/Alchemy/BOPA_Name_Translation.csv
 #    A list of intensity files; may have older SNP names
-FILE=/Users/emilyvonderharr/Dropbox_Morrell_Lab/Genomic_Predictions/alchemy_genotype_calling/Genome_Studio_Files.txt
+FILE=/home/morrellp/vonde026/Alchemy/2008_Plate_MN_ND_Sample_List.txt
 #    A directory to hold intensity files after SNPs are renamed
-OUT_DIR=/Users/emilyvonderharr/Dropbox_Morrell_Lab/Genomic_Predictions/alchemy_genotype_calling/Alchemy_Input
+OUT_DIR=/home/morrellp/vonde026/Alchemy
 #    Specify the path to the Python program
-TRANSLATE_GS_SNP_NAMES=/Users/emilyvonderharr/Documents/Github/Deleterious_GP/Analysis_Scripts/Data_Handling/Translate_GS_SNP_Names.py
+TRANSLATE_GS_SNP_NAMES=/home/morrellp/vonde026/Deleterious_GP/Analysis_Scripts/Data_Handling/Translate_GS_SNP_Names.py
 #    Read the file list into an array
 GSFP=($(cat "${Sample_List}"))
 
