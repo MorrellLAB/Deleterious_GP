4cc7b445ee76f907f29e6fa3e519f3e680cc0cdc
Tom Kono <konox006@umn.edu>
2017-10-18 22:27:11 -0500

    Add C2 and C3 imputation

--- Analysis_Scripts/Genetic_Analysis/FSFHap_Imputation.sh
+++ Analysis_Scripts/Genetic_Analysis/FSFHap_Imputation.sh
@@ -15,15 +15,15 @@
 # with PLINK 1.9, and having only numeric chromosome names.
 
 # Define paths to the input data
-GP_PED="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/Test_C1_Fams.ped"
-GP_MAP="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/Test_C1_Fams.map"
-PEDIGREE="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/Test_Pedigree.txt"
+GP_PED="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/C3_Imputation/C3_Families.ped"
+GP_MAP="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/C3_Imputation/C3_Families.map"
+PEDIGREE="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/C3_Imputation/C3_Pedigree.txt"
 
 # Define paths to the output data
 YMD=$(date +%F)
-OUT_DIR="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/${YMD}"
-LOG="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/FSFHap_${YMD}.log"
-OUT_FNAME="Test_C1_FSFHap.hmp.txt"
+OUT_DIR="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/C3_Imputation/${YMD}"
+LOG="/Volumes/DataDisk/Dropbox/GitHub/Deleterious_GP/Data/FSFHap_Testing/C3_Imputation/FSFHap_${YMD}.log"
+OUT_FNAME="C3_FSFHap.hmp.txt"
 
 # Define paths to the TASSEL pipeline script
 TASSEL="/Volumes/DataDisk/Software/tassel-5-standalone/run_pipeline.pl"
